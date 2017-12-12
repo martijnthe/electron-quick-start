7d612fcbd4372e9ce53d192f6822c9bf87fdcc49
Martijn The <martijn.the@intel.com>
2017-12-12 10:42:26 +0100

    Minimal repro case for https://github.com/electron-userland/electron-packager/issues/774

--- packages/electron-app/main.js
+++ packages/electron-app/main.js
@@ -7,6 +7,9 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+// Will throw because is-number is not packaged!
+const isNumber = require('is-number')
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
